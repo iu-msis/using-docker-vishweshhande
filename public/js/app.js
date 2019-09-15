a614103026e9143f29dcac26bd2683aa3f7e9717
vishweshhande <37101646+vishwesh-hande@users.noreply.github.com>
2019-09-14 20:43:37 -0400

    Added randomuser.api fetch, additional formatting.

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+var randomUsers = new Vue({
+  el: '#randomUsersApp',
+  data: {
+  "users": [{
+    name:{
+      title:"",
+      first:"",
+      last:""
+    },
+
+    location:{
+      state:""
+    },
+
+    email:"",
+    dob:{
+      date:"",
+      age:""
+    },
+    picture:{
+      large:"",
+      thumbnail:""
+
+    }
+
+
+  }]
+},
+
+methods: {
+  fetchUsers(){
+    fetch('https://randomuser.me/api/')
+    .then(function(response) {return response.json() })
+    .then(json => {randomUsers.users = json.results });
+  }
+},
+
+created(){
+  this.fetchUsers();
+}
+})
